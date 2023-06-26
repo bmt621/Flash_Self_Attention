2c09cedf8c8f9065473d42f0b8e852596a5983ac
bmt621 <bmtukur621@gmail.com>
2023-06-26 11:03:43 +0100

    added scalable encoder and decoder block

--- transformers.py
+++ transformers.py
@@ -1,1 +1,13 @@
 from AttentionBlock import *
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self,configs):
+        super(TransformerEncoder, self)
+        pass
+
+class TransformerDecoder(nn.Module):
+    def __init__(self) -> None:
+        super(TransformerDecoder, self).__init__()
+        pass
+
